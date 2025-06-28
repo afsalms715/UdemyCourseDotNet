5aa4a300ef15878e082695752ef3a0f5e42db2bc
afsal ms <afsalms715@gmail.com>
2025-06-28 22:18:16 +0530

    largest of 3 number

--- UdemyCourseDotNet/Program.cs
+++ UdemyCourseDotNet/Program.cs
@@ -19,8 +19,16 @@ secondsToMinutes.ConvertSecondsToMinutes(int.TryParse(Console.ReadLine(), out se
 Console.WriteLine("The time is: " + secondsToMinutes.ConvertSecondsToMinutes(seconds));
 */
 
-HeightCategory heightCategory = new();
-double heightInInches = 0;
-Console.WriteLine("Enter the height in inches: ");
-heightCategory.GetHeightCategory(double.TryParse(Console.ReadLine(), out heightInInches) ? heightInInches : 0);
-Console.WriteLine("The height category is: " + heightCategory.GetHeightCategory(heightInInches));
+//HeightCategory heightCategory = new();
+//double heightInInches = 0;
+//Console.WriteLine("Enter the height in inches: ");
+//heightCategory.GetHeightCategory(double.TryParse(Console.ReadLine(), out heightInInches) ? heightInInches : 0);
+//Console.WriteLine("The height category is: " + heightCategory.GetHeightCategory(heightInInches));
+
+LargestOf3Number largestOf3Number = new LargestOf3Number();
+int number1 = 0, number2 = 0, number3 = 0;
+Console.WriteLine("Enter 3 numbers:");
+number1 = int.TryParse(Console.ReadLine(), out number1) ? number1 : 0;
+number2 = int.TryParse(Console.ReadLine(), out number2) ? number2 : 0;
+number3 = int.TryParse(Console.ReadLine(), out number3) ? number3 : 0;
+largestOf3Number.FindLargestOf3Number(number1, number2, number3);
